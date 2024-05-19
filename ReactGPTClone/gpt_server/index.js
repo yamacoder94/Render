0653f241e93ca8a58ca68b05219d1aadbb892934
yamacoder94 <yamacoder94@gmail.com>
2024-05-19 14:57:56 -0600

    adding model

--- ReactGPTClone/gpt_server/index.js
+++ ReactGPTClone/gpt_server/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
 
+const io = new Server(server);
+
 const PORT = process.env.PORT || 8080;
 
-app.get("/test", (_, res) => {
-  res.send("This is a test");
-});
+// app.get("/test", (_, res) => {
+//   res.send("This is a test");
+// });
 
 server.listen(PORT, () => {
   console.log("Server started on port %d", PORT);
