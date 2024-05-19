a2110d8152c3413c057525d1928ca4ef5d2107be
yamacoder94 <yamacoder94@gmail.com>
2024-05-19 15:06:19 -0600

    adding front end

--- ReactGPTClone/gpt_server/index.js
+++ ReactGPTClone/gpt_server/index.js
@@ -1,6 +1,17 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+//import {fileURLToPath} from "url";
+//import path from "path";
+import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const model = new LlamaModel({
+  modelPath: path.join(__dirname, "models", "codellama-13b.Q3_K_M.gguf"),
+});
+const context = new LlamaContext({ model });
+const session = new LlamaChatSession({ context }); //what is needed to ask any question to the llama model
 
 const app = express();
 const server = createServer(app);
