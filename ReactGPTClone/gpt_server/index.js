5fd185a4a85648ff29d36ea32b08cc532678478a
yamacoder94 <yamacoder94@gmail.com>
2024-05-19 23:26:17 -0600

    adding what is needed to connect with llm on index.js

--- ReactGPTClone/gpt_server/index.js
+++ ReactGPTClone/gpt_server/index.js
@@ -1,19 +1,19 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-// import path from "node:path";
-// import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "url";
 // import cors from "cors";
 
-//import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
+import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
 
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// const model = new LlamaModel({
-//   modelPath: path.join(__dirname, "models", "notus-7b-v1.Q4_K_M.gguf"),
-// });
-// const context = new LlamaContext({ model });
-// const session = new LlamaChatSession({ context }); //what is needed to ask any question to the llama model
+const model = new LlamaModel({
+  modelPath: path.join(__dirname, "models", "notus-7b-v1.Q4_K_M.gguf"),
+});
+const context = new LlamaContext({ model });
+const session = new LlamaChatSession({ context }); //what is needed to ask any question to the llama model
 
 const app = express();
 const server = createServer(app); // Pass the app to the server
